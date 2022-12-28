285b000e40c816827211921ecef6aa2b63c5dd08
yashasvini <ysmarina1234@gmail.com>
2022-12-28 22:01:09 +0530

    Minor improvements

--- main.cpp
+++ main.cpp
@@ -2,13 +2,24 @@
 #include "expression.h"
 
 int main(int argc, char **argv)
-{
+{   std::string help = "Calculator\nDescription: Evaluates arithmetic expressions\n"
+                       "Author: Vipul Kumar\n"
+                       "commands:\n\thelp: !help\n\texit: !exit";
 
     if (argc >= 2)
     {
         std::string expression_str;
         for (int i = 1; i < argc; i++)
             expression_str += argv[i];
+
+        if(expression_str == "!help")
+        {
+            std::cout << help << std::endl;
+            return 0;
+        }
+        if(expression_str == "!exit"){
+            std::cout << "Bye!!" << std::endl;
+        }
 
         expression e(expression_str);
         e.evaluate();
@@ -29,17 +40,11 @@ int main(int argc, char **argv)
             if (input[0] == '!')
             {
                 if (input.substr(1) == "help")
-                {
-                    std::cout << "clc" << std::endl;
-                    std::cout << "description: evaluates arithmetic expressions" << std::endl;
-                    std::cout << "author: Vipul Kumar" << std::endl;
-                    std::cout << "\ncommands:\n\thelp\n\texit" << std::endl;
-                    
-                }
+                    std::cout << help << std::endl;
 
                 else if (input.substr(1) == "exit")
                 {
-                    std::cout << "bye" << std::endl;
+                    std::cout << "Bye!!" << std::endl;
                     exit(0);
                 }
                 continue;
